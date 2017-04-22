94b847b584ff4cc6d0fdae4cf1a16400af4f070f
bchartier <benjamin.chartier@cegetel.net>
2017-04-22 10:07:03 +0200

    update the layers list (for saint-quentin)

--- demos/saint_quentin/map_options.js
+++ demos/saint_quentin/map_options.js
@@ -1,24 +1,23 @@
 var map_options = {
 	"base_map_layers": {
     "Scan25 IGN": new L.tileLayer.wms(
-	    		"http://www.geopicardie.fr/geoserver/wms?", {
-					layers: 'geopicardie:picardie_scan25',
-					format: 'image/jpeg',
-					attribution: "Scan25 - IGN"
-				}),
+        "http://www.geopicardie.fr/geoserver/wms?", {
+            layers: 'geopicardie:picardie_scan25',
+            format: 'image/jpeg',
+            attribution: "Scan25 - IGN"
+        }),
     "Images aériennes": new L.tileLayer.wms(
-    		"http://www.geopicardie.fr/geoserver/wms?", {
-					layers: 'agglo_st_quentin:agglo_st_quentin_ortho_2012_vis',
-					format: 'image/jpeg',
-					attribution: "GéoPicardie"
-				}),
-    "Open Mapquest": new L.TileLayer(
-				'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-					maxZoom: 18,
-					attribution: '<a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>,<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.',
-					subdomains: ['otile1','otile2','otile3','otile4'],
-					format: 'image/jpeg'
-				})
+        "http://www.geopicardie.fr/geoserver/wms?", {
+                layers: 'agglo_st_quentin:agglo_st_quentin_ortho_2012_vis',
+                format: 'image/jpeg',
+                attribution: "GéoPicardie"
+            }),
+    "OpenStreetMap - GéoPicardie": new L.tileLayer.wms(
+        "http://osm.geopicardie.fr/mapproxy/service?", {
+          layers: 'bright',
+          format: 'image/jpeg',
+          attribution: "OpenStreetMap - GéoPicardie"
+        })
 	},
 	"init_view": {
 		"center": [49.8460,3.3110],
